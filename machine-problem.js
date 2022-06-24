0c31e14e2dd6d39f4f9cdaec9720a161fa04225e
Pirc <bennington12323@gmail.com>
2022-06-23 22:11:00 -0700

    90% done..

--- machine-problem.js
+++ machine-problem.js
@@ -1,31 +1,25 @@
 let inputs;
 const allBtns = document.querySelectorAll('button');
 const buttonSubmit = document.querySelector('#submit');
-const buttonSubmit2 = document.querySelector('.submit2');
-const buttonSubmit3 = document.querySelector('.submit3')
+// const buttonSubmit2 = document.querySelector('.submit2');
+// const buttonSubmit3 = document.querySelector('.submit3')
 let inputField = document.querySelector('.inputs');
-let inputField2 = document.querySelector('.inputs2');
-let inputField3 = document.querySelector('.inputs3');
 const outputContainer = document.querySelector('.output-container');
-const outputContainer2 = document.querySelector('.output-container2');
-const outputContainer3 = document.querySelector('.output-container3')
-// const box = document.querySelector('.box');
+const oddNumber = document.querySelector('.oddNumber');
 
-let brows = 5;
-let bcolumns = 5;
 
-function boxes(){
-    let 
-}
 
 // Size of 3
 // Number of lines height
-let height = 3;
+
 // Number of character width in each line
-let width = (2 * height) - 1;
+
 let pixel = 40;
 
-const printX = (b) => {
+const printX = (b,height) => {
+    b.style.width = (39 * height) + 'px';
+    b.style.height = (39 * height) + 'px';
+    let width = (2 * height) - 1;
     const box = document.querySelector('.box');
     let i, j, counter = 0;
     for (let i = 0; i < height; i++) {
@@ -61,9 +55,72 @@ const printX = (b) => {
     counter++;
   
 }
+// PRINT Y
+// const printY = (b,height) => {
+//     // let width = (2 * height) - 1;
+//     // let i, j, counter = 0;
+// for (let i = 0; i < height; i++) {
+//     for (let j = 0; j < height; j++) {
+//       const boxElem = document.createElement("div");
+//       boxElem.classList.add("box-item");
 
-const printY = (b) => {
-    
+//       const y = `${i * pixel}px`;
+//       const x = `${j * pixel}px`;
+
+//       boxElem.style.top = y;
+//       boxElem.style.left = x;
+//       boxElem.innerText = "o";
+//       boxElem.style.textAlign = 'center';
+//       boxElem.style.width = '24px';
+//       boxElem.style.height = '24px';
+
+//       let halfOfTheInput = Math.round(height / 2);
+
+//       if (Math.round(height / 2) > i) {
+//         if (i == j || i + j == height - 1) {
+//           b.appendChild(boxElem);
+//         }
+//       } else {
+//         boxElem.style.left = `${(halfOfTheInput - 1) * pixel}px`;
+//         parentElem.appendChild(boxElem);
+//         console.log({ i, j });
+//       }
+//     }
+//   }
+// }
+// ;
+
+// XXXX
+// const printY = (parentElem,height) => {
+//     for (let i = 0; i < height; i++) {
+//       for (let j = 0; j < height; j++) {
+//         const boxElem = document.createElement("div");
+//         boxElem.classList.add("box-item");
+  
+//         const y = `${i * pixel}px`;
+//         const x = `${j * pixel}px`;
+  
+//         boxElem.style.top = y;
+//         boxElem.style.left = x;
+//         boxElem.innerText = "o";
+  
+//         let halfOfTheInput = Math.round(height / 2);
+  
+//         if (Math.round(height / 2) > i) {
+//           if (i == j || i + j == height - 1) {
+//             parentElem.appendChild(boxElem);
+//           }
+//         } else {
+//           boxElem.style.left =` ${(halfOfTheInput - 1) * pixel}px`;
+//           parentElem.appendChild(boxElem);
+//         }
+//       }
+//     }
+//   };
+const printY = (b,height) => {
+    b.style.width = (39 * height) + 'px';
+    b.style.height = (39 * height) + 'px';
+    let width = (2 * height) - 1;
     let i, j, counter = 0;
     for (i = 0; i < height; i++) {
         for (j = 0; j < height; j++) {
@@ -80,7 +137,7 @@ const printY = (b) => {
             div.style.height = '24px';
             div.style.textAlign = 'center';
             div.style.position = 'absolute';
-            if (j == counter || j == height - counter  && i <= parseInt(height / 2)){
+            if (j == counter || i + j == height - counter  && i <= parseInt(height / 2)){
                 div.innerHTML = 'o';
                 console.log('o');
                 b.append(div);
@@ -96,7 +153,10 @@ const printY = (b) => {
     }
 }
 
-const printZ = (b) => {
+const printZ = (b,height) => {
+    b.style.width = (39 * height) + 'px';
+    b.style.height = (39 * height) + 'px';
+    let width = (2 * height) - 1;
     let i, j, counter = height - 1;
     for (i = 0; i < height; i++) {
         for (j = 0; j < height; j++) {
@@ -134,40 +194,67 @@ buttonSubmit.addEventListener('click', getInput);
 
 function getInput(){
     let p = []
-    let inputFields = document.querySelector('.inputs').value.toUpperCase().split('');
-    console.log(inputFields);
-    for(let i = 0; i < inputFields.length; i++){
+    let inputOddNumber = parseInt(document.querySelector('.oddNumber').value);
+    let inputDirection = document.querySelector('.direction').value.toLowerCase();
+    let inputText = document.querySelector('.inputs').value.toUpperCase().split('');
+    console.log(inputText);
+    console.log(inputOddNumber)
+    let elemFlow = setDir(inputDirection);
+    console.log(elemFlow)
+    // if()
+
+
+    for(let i = 0; i < inputText.length; i++){
         
-        if(inputFields[i] === 'X'){
+        if(inputText[i] === 'X'){
+
+            outputContainer.style.flexDirection = `${elemFlow}`;
+            let height = inputOddNumber;
             const divBox = document.createElement('div');
             divBox.classList.add('box');
             outputContainer.append(divBox)
-            p.push(inputFields[i])
+            p.push(inputText[i])
             console.log(p)
             console.log(p.length)
-            printX(divBox);
+            printX(divBox,height);
     
-        }else if(inputFields[i] === 'Y'){
+        }else if(inputText[i] === 'Y'){
+
+            outputContainer.style.flexDirection = `${elemFlow}`;
+            let height = inputOddNumber;
             const divBox = document.createElement('div');
             divBox.classList.add('box');
             outputContainer.append(divBox)
-            p.push(inputFields[i])
+            p.push(inputText[i])
             console.log(p)
             console.log(p.length)
-            printY(divBox);
+            printY(divBox,height);
            
-        }else if(inputFields[i] === 'Z'){
+        }else if(inputText[i] === 'Z'){
+
+            outputContainer.style.flexDirection =   `${elemFlow}`;
+            let height = inputOddNumber;
             const divBox = document.createElement('div');
             divBox.classList.add('box');
             outputContainer.append(divBox)
-            p.push(inputFields[i])
+            p.push(inputText[i])
             console.log(p)
             console.log(p.length)
-            printZ(divBox);
+            printZ(divBox,height);
         }
         // return inputFields;
     }
 }
+
+   function setDir(dir){
+        let directions = '';
+        if(dir === 'vertical'){
+            return directions = 'row'
+        }else{
+            return directions = 'column'
+        };
+        
+    };
 
 // allBtns.forEach(btns => {
 //     btns.addEventListener('click', function (e){
@@ -184,151 +271,151 @@ function getInput(){
 
 // Size of 5
 // Number of lines height
-let heighthor = 5;
-// Number of character width in each line
-let widthHor = (2 * heighthor) - 1;
-let pixelHor = 21;
+// let heighthor = 5;
+// // Number of character width in each line
+// let widthHor = (2 * heighthor) - 1;
+// let pixelHor = 21;
 
-const printXhor = (b) => {
-    const box = document.querySelector('.box');
-    let i, j, counter = 0;
-    for (let i = 0; i < heighthor; i++) {
-        console.log(i)
-     for (let j = 0; j < heighthor; j++) {
-        let newI = i * pixelHor;
-        let newJ = j * pixelHor;
+// const printXhor = (b) => {
+//     const box = document.querySelector('.box');
+//     let i, j, counter = 0;
+//     for (let i = 0; i < heighthor; i++) {
+//         console.log(i)
+//      for (let j = 0; j < heighthor; j++) {
+//         let newI = i * pixelHor;
+//         let newJ = j * pixelHor;
         
-        let div = document.createElement('div');
-        // div.style.border = '1px solid greenyellow';
-        div.style.top = newI + 'px';
-        div.style.left = newJ + 'px';
+//         let div = document.createElement('div');
+//         // div.style.border = '1px solid greenyellow';
+//         div.style.top = newI + 'px';
+//         div.style.left = newJ + 'px';
         
-        div.style.fontFamily = 'inherit';
-        div.style.width = '24px';
-        div.style.height = '24px';
-        div.style.textAlign = 'center';
-        div.style.position = 'absolute';
-        // box.append(div)
-         if (i == j || i + j == heighthor - 1 ){
-    // document.write("o");
-            div.innerHTML = 'o';
-            console.log('o');
-       b.append(div);
+//         div.style.fontFamily = 'inherit';
+//         div.style.width = '24px';
+//         div.style.height = '24px';
+//         div.style.textAlign = 'center';
+//         div.style.position = 'absolute';
+//         // box.append(div)
+//          if (i == j || i + j == heighthor - 1 ){
+//     // document.write("o");
+//             div.innerHTML = 'o';
+//             console.log('o');
+//        b.append(div);
         
-      }else{
-        console.log(' ');
-         box.append(div);
-    }
-        }
+//       }else{
+//         console.log(' ');
+//          box.append(div);
+//     }
+//         }
  
-    }
-    counter++;
+//     }
+//     counter++;
   
-}
+// }
 
-const printYhor = (b) => {
+// const printYhor = (b) => {
     
-    let i, j, counter = 0;
-    for (i = 0; i < heighthor; i++) {
-        for (j = 0; j < heighthor; j++) {
-            let newI = i * pixelHor;
-            let newJ = j * pixelHor;
+//     let i, j, counter = 0;
+//     for (i = 0; i < heighthor; i++) {
+//         for (j = 0; j < heighthor; j++) {
+//             let newI = i * pixelHor;
+//             let newJ = j * pixelHor;
             
-            let div = document.createElement('div');
-            div.style.border = '1px solid greenyellow';
-            div.style.top = newI + 'px';
-            div.style.left = newJ + 'px';
-            div.style.margin = '4px';
-            div.style.fontFamily = 'inherit';
-            div.style.width = '24px';
-            div.style.height = '24px';
-            div.style.textAlign = 'center';
-            div.style.position = 'absolute';
-            if (j == counter || j == heighthor - counter  && i <= parseInt(heighthor / 2)){
-                div.innerHTML = 'o';
-                console.log('o');
-                b.append(div);
-                // box.append(div);
-            }else{
-                div.innerHTML = ' ';
-                b.append(div);
-            }
-        }
-        if (i < parseInt(heighthor / 2))
-            counter++;
-    }
-}
+//             let div = document.createElement('div');
+//             div.style.border = '1px solid greenyellow';
+//             div.style.top = newI + 'px';
+//             div.style.left = newJ + 'px';
+//             div.style.margin = '4px';
+//             div.style.fontFamily = 'inherit';
+//             div.style.width = '24px';
+//             div.style.height = '24px';
+//             div.style.textAlign = 'center';
+//             div.style.position = 'absolute';
+//             if (j == counter || j == heighthor - counter  && i <= parseInt(heighthor / 2)){
+//                 div.innerHTML = 'o';
+//                 console.log('o');
+//                 b.append(div);
+//                 // box.append(div);
+//             }else{
+//                 div.innerHTML = ' ';
+//                 b.append(div);
+//             }
+//         }
+//         if (i < parseInt(heighthor / 2))
+//             counter++;
+//     }
+// }
 
-const printZhor = (b) => {
-    let i, j, counter = heighthor - 1;
-    for (i = 0; i < heighthor; i++) {
-        for (j = 0; j < heighthor; j++) {
-            let newI = i * pixelHor;
-            let newJ = j * pixelHor;
+// const printZhor = (b) => {
+//     let i, j, counter = heighthor - 1;
+//     for (i = 0; i < heighthor; i++) {
+//         for (j = 0; j < heighthor; j++) {
+//             let newI = i * pixelHor;
+//             let newJ = j * pixelHor;
             
-            let div = document.createElement('div');
-            // div.style.border = '1px solid greenyellow';
-            div.style.top = newI + 'px';
-            div.style.left = newJ + 'px';
-            div.style.margin = '4px';
-            div.style.fontFamily = 'inherit';
-            div.style.width = '24px';
-            div.style.height = '24px';
-            div.style.textAlign = 'center';
-            div.style.position = 'absolute';
+//             let div = document.createElement('div');
+//             // div.style.border = '1px solid greenyellow';
+//             div.style.top = newI + 'px';
+//             div.style.left = newJ + 'px';
+//             div.style.margin = '4px';
+//             div.style.fontFamily = 'inherit';
+//             div.style.width = '24px';
+//             div.style.height = '24px';
+//             div.style.textAlign = 'center';
+//             div.style.position = 'absolute';
        
-        if (i == 0 || i == heighthor - 1 || j == counter){
-            div.innerHTML = 'o'
-             console.log('o');
-             b.append(div);
+//         if (i == 0 || i == heighthor - 1 || j == counter){
+//             div.innerHTML = 'o'
+//              console.log('o');
+//              b.append(div);
             
-           } else{
-            div.innerHTML = ' ';
-            b.append(div);
-           }
+//            } else{
+//             div.innerHTML = ' ';
+//             b.append(div);
+//            }
                 
-        }
-        counter--;
-        // document.write(`<br/>`);
-    }
-}
-buttonSubmit2.addEventListener('click', getInputHorizontal);
+//         }
+//         counter--;
+//         // document.write(`<br/>`);
+//     }
+// }
+// buttonSubmit2.addEventListener('click', getInputHorizontal);
 
 
-function getInputHorizontal(){
-    console.log('horizontal')
-    let p = []
-    let inputFields2 = document.querySelector('.inputs2').value.toUpperCase().split('');
-    console.log(inputFields2);
-    for(let i = 0; i < inputFields2.length; i++){
+// function getInputHorizontal(){
+//     console.log('horizontal')
+//     let p = []
+//     let inputFields2 = document.querySelector('.inputs2').value.toUpperCase().split('');
+//     console.log(inputFields2);
+//     for(let i = 0; i < inputFields2.length; i++){
         
-        if(inputFields2[i] === 'X'){
-            const divBox2 = document.createElement('div');
-            divBox2.classList.add('box');
-            outputContainer2.append(divBox2)
-            p.push(inputFields2[i])
-            console.log(p)
-            console.log(p.length)
-            printXhor(divBox2);
+//         if(inputFields2[i] === 'X'){
+//             const divBox2 = document.createElement('div');
+//             divBox2.classList.add('box');
+//             outputContainer2.append(divBox2)
+//             p.push(inputFields2[i])
+//             console.log(p)
+//             console.log(p.length)
+//             printXhor(divBox2);
     
-        }else if(inputFields2[i] === 'Y'){
-            const divBox2 = document.createElement('div');
-            divBox2.classList.add('box');
-            outputContainer2.append(divBox2)
-            p.push(inputFields2[i])
-            console.log(p)
-            console.log(p.length)
-            printYhor(divBox2);
+//         }else if(inputFields2[i] === 'Y'){
+//             const divBox2 = document.createElement('div');
+//             divBox2.classList.add('box');
+//             outputContainer2.append(divBox2)
+//             p.push(inputFields2[i])
+//             console.log(p)
+//             console.log(p.length)
+//             printYhor(divBox2);
            
-        }else if(inputFields2[i] === 'Z'){
-            const divBox2 = document.createElement('div');
-            divBox2.classList.add('box');
-            outputContainer2.append(divBox2)
-            p.push(inputFields2[i])
-            console.log(p)
-            console.log(p.length)
-            printZhor(divBox2);
-        }
-        // return inputFields;
-    }
-}
+//         }else if(inputFields2[i] === 'Z'){
+//             const divBox2 = document.createElement('div');
+//             divBox2.classList.add('box');
+//             outputContainer2.append(divBox2)
+//             p.push(inputFields2[i])
+//             console.log(p)
+//             console.log(p.length)
+//             printZhor(divBox2);
+//         }
+//         // return inputFields;
+//     }
+// }
